f74c9192c50e93a746d2e0ba7eadab4ad156b77f
Barakat Oseni <99281587+Barakat-Oseni@users.noreply.github.com>
2022-06-02 20:17:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import random
+
+from regex import R
+user_name = input('Your Username? ')
+available_options = ['R','S','P']
+print('The available options are:',available_options)
+print('R-Rock, S-Scissors, P-Paper')
+sys_choice = random.choice(available_options)
+
+#To get the user input 
+def user_input():
+    global user_choice
+    user_choice = input('Pick a choice? ')
+    user_choice = user_choice.upper().strip()
+    if user_choice in available_options:
+        my_choice(user_choice,sys_choice)
+    else:
+        print('Wrong choice, please try again!')
+        user_input()
+
+#To check conditions on the user choice and the CPU choice
+def my_choice(user_choice, sys_choice):
+    if user_choice == sys_choice:
+        print(f'{user_name}({user_choice}) : CPU({sys_choice})')
+        print('Same choices!!!')
+        user_input()
+    else:
+        other_choices(user_choice,sys_choice)
+
+def other_choices(user_choice, sys_choice):
+    print(f'{user_name}({user_choice}) : CPU({sys_choice})')
+    if user_choice == 'R':
+        if sys_choice == 'S':
+            print('You won! Rock smashes scissors')
+        else:
+            print('you lost! Paper covers rock')
+
+    if user_choice == 'S':
+        if sys_choice == 'P':
+            print('You won! Scissors cuts paper')
+        else:
+            print('you lost! Rock smashes scissors')
+
+    if user_choice == 'P':
+        if sys_choice == 'R':
+            print('You won! Paper covers Rock')
+        else:
+            print('you lost! Scissors cuts paper')
+            
+
+while True:
+    user_input()
+    play_again = input('Do you want to play again? Y/N')
+    play_again = play_again.lower().strip()
+    if play_again == 'n':
+        break
